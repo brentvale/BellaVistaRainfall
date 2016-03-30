2c2108d12397e0f05336c1ec1e2fb7e276cc9615
Brent Vale <brentvale@gmail.com>
2016-03-30 06:57:56 -0700

    adds react/flux folder structure and files to javascripts

---
+++ app/assets/javascripts/constants/rain_constants.js
@@ -0,0 +1,3 @@
+RainfallConstants = {
+  
+}
---
+++ app/assets/javascripts/stores/rainfall.js
@@ -0,0 +1,8 @@
+(function(root){
+  var CHANGE_EVENT = "change";
+  
+  root.RainfallStore = $.extend({}, EventEmitter.prototype, {
+
+  })
+  
+})(this)
